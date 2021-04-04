e275da2de48b74eecb52bf5c86b8e3eba8cabdcd
SW <aliwo@naver.com>
2021-04-04 17:21:09 +0900

    journal 생성시 duplicate 차단

--- api/rest/journal.py
+++ api/rest/journal.py
@@ -4,7 +4,9 @@ from flask import g, request
 from sqlalchemy import extract
 
 from api.models.journal import Journal
+from api.models.user import User
 from libs.database.engine import afr, Session
+from libs.route.errors import ClientError
 from libs.route.router import route
 from libs.status import Status
 
@@ -21,14 +23,29 @@ def get_all_journals_of_month():
 
 @route
 def create_journal():
+    '''
+    유저 하나에 락을 걸어야 되는거 같은데
+    락을 걸고 특정 날짜 + 특정 유저 + 특정 routine_id 가 중복되는 row 가 있는지 찾고, 만약 있다면 추가하면 안 됨
+    '''
+    date = datetime.strptime(request.json.get('date'), '%Y-%m-%d').date()
+    user = Session().query(User).filter_by(id=g.user_session.user.id).with_for_update().one()
+    duplicate = Session().query(Journal).filter(
+        (Journal.user_id == g.user_session.user.id)
+        & (Journal.routine_id == request.json.get('routine_id'))
+        & (Journal.date == date)
+    ).first()
+
+    if duplicate:
+        raise ClientError('duplicate journal')
+
     afr(
         Journal(user_id=g.user_session.user.id,
                 routine_id=request.json.get('routine_id'),
-                date=datetime.strptime(request.form.get('date'), '%Y-%m-%d').date()
+                date=date
         )
     )
     Session().commit()
-    return {'okay'}, Status.HTTP_200_OK
+    return {'okay': True}, Status.HTTP_200_OK
 
 
 
