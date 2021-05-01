7f7042bfadb6d99b8af4a485f50d9aef3a8be67a
SW <aliwo@naver.com>
2021-05-01 22:30:14 +0900

    사진 API 정비
    사진 API 가 작동 안하고 있었당~

--- api/models/picture.py
+++ api/models/picture.py
@@ -7,16 +7,14 @@ from libs.database.types import Base
 class Picture(Base):
     __tablename__ = 'pictures'
     user_id = Column(Integer, ForeignKey('users.id', ondelete='SET NULL'), index=True)
-    date = Column(DATE)
     url = Column(TEXT)
 
     def __init__(self, **kwargs):
-        super(Picture, self).__init__()
+        super(Picture, self).__init__(**kwargs)
 
     def json(self):
         return {
             'id': self.id,
             'user_id': self.user_id,
-            'date': self.date.strftime("%Y-%m-%d"),
             'url': self.url,
         }
--- api/rest/picture.py
+++ api/rest/picture.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from api.models.picture import Picture
 from libs.database.engine import afr, Session
+from libs.route.errors import ClientError
 from libs.route.router import route
 from libs.status import Status
 from libs.storage import gcs, s3
@@ -14,4 +15,16 @@ def upload_picture():
     url = s3.upload_file(file, file.filename)
     picture = afr(Picture(user_id=g.user_session.user.id, url=url))
     Session().commit()
+    return picture.json(), Status.HTTP_200_OK
+
+
+@route
+def get_picture_by_id(picture_id):
+    try:
+        picture = Session().query(Picture).filter_by(id=picture_id).one()
+    except:
+        raise ClientError(f'picture id #{picture_id} not found', Status.HTTP_404_NOT_FOUND)
+    if picture.user_id != g.user_session.user.id:
+        raise ClientError('not your picture')
+
     return picture.json(), Status.HTTP_200_OK
